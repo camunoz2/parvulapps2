0492ef8d6f5d4402cf5dbf902618c3aff288345a
Cristian M <camunoz2@gmail.com>
2024-03-29 12:48:32 -0300

    added protected route

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,14 @@
 import { AddCourse } from "@/components/add-course";
 import { SideBar } from "@/components/sidebar";
+import { withPageAuthRequired, getSession } from '@auth0/nextjs-auth0';
 
-export default function DashboardPage() {
+export default withPageAuthRequired(async function DashboardPage() {
+  const { user } = await getSession()
   return (
     <div className="grid grid-cols-2">
       <SideBar />
       <AddCourse />
     </div>
   )
-}
+}, { returnTo: "/dashboard" })
+
