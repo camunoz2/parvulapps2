e2df0b6f31bb23a9a04828123a85de92b1101162
Cristian M <camunoz2@gmail.com>
2024-04-07 16:23:48 -0400

    GET schools from db

--- src/app/dashboard/admin/page.tsx
+++ src/app/dashboard/admin/page.tsx
@@ -21,8 +21,36 @@ import {
 	DropdownMenuItem,
 } from "@/components/ui/dropdown-menu";
 import { PlusCircle, MoreHorizontal } from "lucide-react";
+import { turso } from "@/lib/turso";
 
-export default function AdminPage() {
+export interface Schools {
+	id: number;
+	name: string;
+	address: string;
+	city: string;
+	state: string;
+	domain: string;
+	admin_email: string;
+	admin_name: string;
+}
+
+async function getSchools() {
+	try {
+		const res = await turso.execute("SELECT * FROM schools");
+		return {
+			schools: res.rows as unknown as Schools[],
+		};
+	} catch (error) {
+		console.error(error);
+		return {
+			schools: [],
+		};
+	}
+}
+
+export default async function AdminPage() {
+	const { schools } = await getSchools();
+
 	return (
 		<div className="flex min-h-screen w-full flex-col">
 			<main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-6">
@@ -55,36 +83,38 @@ export default function AdminPage() {
 								</TableRow>
 							</TableHeader>
 							<TableBody>
-								<TableRow>
-									<TableCell>
-										<div className="font-medium">Colegio Demo</div>
-										<div className="hidden text-sm text-muted-foreground md:inline">
-											Calle San Valentin 8989
-										</div>
-									</TableCell>
-									<TableCell>Emilio</TableCell>
-									<TableCell>emilio@asd.cl</TableCell>
-									<TableCell>asd.cl</TableCell>
-									<TableCell>23</TableCell>
-									<TableCell>
-										<DropdownMenu>
-											<DropdownMenuTrigger asChild>
-												<Button
-													aria-haspopup="true"
-													size="icon"
-													variant="ghost"
-												>
-													<MoreHorizontal className="h-4 w-4" />
-													<span className="sr-only">Toggle menu</span>
-												</Button>
-											</DropdownMenuTrigger>
-											<DropdownMenuContent align="center">
-												<DropdownMenuItem>Editar</DropdownMenuItem>
-												<DropdownMenuItem>Eliminar</DropdownMenuItem>
-											</DropdownMenuContent>
-										</DropdownMenu>
-									</TableCell>
-								</TableRow>
+								{schools.map((row) => (
+									<TableRow id={row.id.toString()}>
+										<TableCell>
+											<div className="font-medium">{row.name}</div>
+											<div className="hidden text-sm text-muted-foreground md:inline">
+												{row.address}
+											</div>
+										</TableCell>
+										<TableCell>{row.admin_name}</TableCell>
+										<TableCell>{row.admin_email}</TableCell>
+										<TableCell>{row.domain}</TableCell>
+										<TableCell>23</TableCell>
+										<TableCell>
+											<DropdownMenu>
+												<DropdownMenuTrigger asChild>
+													<Button
+														aria-haspopup="true"
+														size="icon"
+														variant="ghost"
+													>
+														<MoreHorizontal className="h-4 w-4" />
+														<span className="sr-only">Toggle menu</span>
+													</Button>
+												</DropdownMenuTrigger>
+												<DropdownMenuContent align="center">
+													<DropdownMenuItem>Editar</DropdownMenuItem>
+													<DropdownMenuItem>Eliminar</DropdownMenuItem>
+												</DropdownMenuContent>
+											</DropdownMenu>
+										</TableCell>
+									</TableRow>
+								))}
 							</TableBody>
 						</Table>
 					</CardContent>
