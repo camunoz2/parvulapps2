b38c4be0ea22f39938b0b21fb5bf412c03184917
Cristian M <camunoz2@gmail.com>
2024-04-12 20:16:39 -0400

    add toast

---
+++ src/components/toast-notification.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { ToastAction } from "@/components/ui/toast";
+import { useToast } from "@/components/ui/use-toast";
+
+export function ToastNotification({
+	description,
+}: {
+	description: string;
+}) {
+	const { toast } = useToast();
+
+	toast({
+		title: "Scheduled: Catch up ",
+		description,
+		action: <ToastAction altText="Goto schedule to undo">Undo</ToastAction>,
+	});
+
+	return <></>;
+}
