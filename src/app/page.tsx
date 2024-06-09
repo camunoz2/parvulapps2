ae3442087a52916f77a52bfacc98330508fd0717
Cristian M <camunoz2@gmail.com>
2024-06-09 09:46:04 -0400

    fix dashboard login after aut

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,6 @@
-import { Avatar } from "@/components/profile-client";
-import { getSession } from "@auth0/nextjs-auth0";
 import { Button } from "@/components/ui/button";
 
 export default async function Home() {
-  const session = await getSession();
-  const user = session?.user;
-
   return (
     <main className=" grid grid-cols-2 h-full">
       <div className="bg-gray-800 flex flex-col justify-between py-10 pl-12">
@@ -20,26 +15,10 @@ export default async function Home() {
         </footer>
       </div>
       <div className="flex flex-col gap-6 items-center justify-center">
-        {user ? (
-          <h2 className="text-2xl font-bold">Bienvenid@ {user.name}</h2>
-        ) : (
-          <h2 className="text-2xl font-bold">Hola! Inicia sesión con google</h2>
-        )}
-        {user ? (
-          <>
-            <Avatar user={user} />
-            <Button className="w-48">
-              <a href="/dashboard">Abrir app</a>
-            </Button>
-            <Button className="w-48" variant="secondary">
-              <a href="/api/auth/logout">Cerrar Sesion</a>
-            </Button>
-          </>
-        ) : (
-          <Button>
-            <a href="/api/auth/login">Iniciar Sesion</a>
-          </Button>
-        )}
+        <h2 className="text-2xl font-bold">Hola! Inicia sesión con google</h2>
+        <Button>
+          <a href="/api/auth/login?returnTo=/dashboard">Iniciar Sesion</a>
+        </Button>
       </div>
     </main>
   );
