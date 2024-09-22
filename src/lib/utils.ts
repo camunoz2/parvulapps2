2f9586099f0c192d951b10dba1ace99dfbc1f71c
Cristian M <camunoz2@gmail.com>
2024-09-21 21:03:27 -0300

    updated: shadcn themes

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,24 +1,6 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-import type { SelectCourse } from "@/db/schema/course";
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs))
 }
-
-export const getCourseNameById = (
-  studentCourseId: number,
-  courses: SelectCourse[],
-) => {
-  // create a map with { courseId : courseName}
-  const courseNameById = new Map<number, string>();
-
-  for (const course of courses) {
-    courseNameById.set(course.id, course.name);
-  }
-
-  return (
-    courseNameById.get(studentCourseId) ||
-    "El estudiante no pertenece a ningun curso"
-  );
-};
