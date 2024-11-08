45b0196c638a0ab3ae9ef7573b254a14377e3352
Cristian M <camunoz2@gmail.com>
2024-11-08 10:04:01 -0300

    fix

--- src/app/dashboard/reports/page.tsx
+++ src/app/dashboard/reports/page.tsx
@@ -7,8 +7,6 @@ export default async function Page() {
   const periods = await getPeriods();
   const allGrades = await getAllGrades();
 
-  console.log(allGrades[0])
-
   return (
     <main className="flex min-h-screen w-full flex-col p-4 gap-4">
       <div className="grid grid-cols-3 gap-4">
